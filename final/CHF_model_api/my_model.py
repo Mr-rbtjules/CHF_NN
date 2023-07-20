8473e61566667444fdc91671a92a78cc8cb31c53
Mr-rbtjules <jules.dumont@yahoo.fr>
2023-07-20 17:39:09 +0200

    s

--- final/CHF_model_api/my_model.py
+++ final/CHF_model_api/my_model.py
@@ -42,7 +42,6 @@ from typing import List
 import CHF_model_api as CHF
 
 
-tf.random.set_seed(CHF.config.SEED_TF)
 
     #seed for data shuffle
 np.random.seed(CHF.config.SEED_NP)
@@ -63,12 +62,15 @@ class My_model:
     def __init__(
             self, 
             hparams:    dict = None,
-            model_name:   str = None
+            model_name:   str = None,
+            auto_save: bool = False
     ) ->None:
         """
-        either create a CHF prediction model from scratch
-        or load one and his hp from a .h5 file
+        either create a CHF prediction model from scratch using hparams
+        or load a saved one from model_name from a .h5 file and his hp 
+        from json
         """
+        self.auto_save = auto_save
         self.hparams = hparams
         self.name = None
         self.X_val = None
@@ -77,22 +79,145 @@ class My_model:
         self.y_train = None
         self.normalization_mean = None
         self.normalization_std = None
-        #to now if model already exist
-        self.now = datetime.now().strftime("%Y%m%d-%H%M%S")
 
-        self.loss_function = MeanSquaredLogarithmicError()
-        self.other_metrics = ['mape', self.batch_nrmse]
         self.model = None
         self.callbacks = None
+        #to compare name and now if model already exist
+        self.now = datetime.now().strftime("%Y%m%d-%H%M%S")
+        self.loss_function = MeanSquaredLogarithmicError()
+        self.actMethod = LeakyReLU
+        self.monitor_param = 'loss'
+        self.other_metrics = ['mape', self.batch_nrmse]
+        
+
+        #load hp and attributes
         self.load_my_model(model_name)
-        
         #ici hparams enfin fini (sauf pour metrics)
+
+    def load_my_model(self, model_name: str)->None:
+        """Create a model based on hparams or
+        use a saved one based on his name, set also all the attributes"""
+        #new
+        if model_name == None:
         
+            self.model = Sequential()
+            self.name = self.now
+            print("Load new model: ", self.name)
+            self.create_new_model()   
+        #model already existing
+        else:
+            path = "./models/" + model_name + ".h5"
+            #load model from files
+            self.model = load_model(path)
+            #load hparams
+            self.hparams = CHF.tools.get_hparams_saved_model(model_name)
+            self.name = self.hparams['name']
+            print("Load saved model: ", self.name)
+
+        #make ready for train
+        print("Model Loaded, init callbacks")
+        self.callbacks = self.init_callbacks()
+        #check if the corresponding vali/train set already computed
+        #set it and the other attributes
+        self._load_data(self.hparams['data_seed'])
+        return None
+    
+    def create_new_model(self)->None:
+        """create new model based on hparams"""
+        #set a defined seed for the weights to be controlled
+        #if we want to reproduce results
+        seed_tf = self.hparams['seed_tf']
+        tf.random.set_seed(seed_tf)
+
+        architecture = self.hparams['architecture']
+        for layer, neurons in enumerate(architecture[1:]):
+            if layer == 0:
+                self.model.add(Dense(
+                    neurons, 
+                    input_shape=(architecture[0],)
+                ))
+            else:
+                self.model.add(Dense(neurons))
     #a tester prc pas sur que tf kiffe
-    def batch_nrmse(self, y_true, y_pred):
-        return K.sqrt(K.mean(K.square(y_pred - y_true)))/K.mean(y_true)
+            self.model.add(Activation(
+                self.actMethod(self.hparams['alpha_acti'])
+            ))
+            if  layer != len(architecture[1:]) - 1 :
+                if self.hparams['batch_normalisation'] == True:
+                    self.model.add(BatchNormalization())  
+            if layer == 0 or layer == 1:
+                self.model.add(Dropout(self.hparams['dropout_rate']))
+        opti = self.hparams['optimizer']
+        if opti == 'adam':
+            opti = Adam
+        else: opti = SGD
+        self.model.compile(
+            optimizer=opti(learning_rate=self.hparams['learning_rate']), 
+            loss=self.loss_function, 
+            metrics=self.other_metrics
+        )
+        return None
 
+    def init_callbacks(self) -> list:
+        """action taken at evry epoch for monitoring"""
+        early_stop = EarlyStopping(
+            monitor='loss',          #loss easier to monitor because less variable
+            min_delta=self.hparams['loss_delta_stop'],   #if nothing precise, it will check loss and stop after no change of >=0.1
+            patience=self.hparams['patience'], 
+            verbose=1,
+            restore_best_weights=True
+        )
+        #schedule == function to give to LearningScheduler, with epoch and lr as param
+        learningRateScheduler = LearningRateScheduler(self.lr_scheduler)
+        logdir = "./logs/hparam_tuning/" + self.name
+        tb_metrics = TensorBoard(logdir)
+        return [early_stop, learningRateScheduler, tb_metrics]
 
+    def train(self) -> dict:
+        history = self.model.fit(
+            self.X_train, 
+            self.y_train,
+            shuffle=False,      #because already shuffled
+            validation_data=(self.X_val,self.y_val),
+            batch_size=self.hparams['batch_size'], 
+            epochs=self.hparams['max_epochs'],
+            callbacks=self.callbacks, #mpa_callback
+            verbose=1
+        )
+        #record result in hparams
+        if self.auto_save:
+            self.save(overwrite=True)
+        return history 
+    #attention once saved we have a new model that exist ~conflict
+    #faire en sorte quapres save on fait en sorte que modele exit deja 
+    #en modifiant now
+    def save(self, overwrite=False)->None: 
+        """save model in .h5 format, to make predictions
+        save also the hparams of the model"""
+
+        if self.name != self.now: #already existing model 
+            if overwrite:
+                #no need to change name, save the model on top on the old
+                #version but change hparams because performance might be diff
+                
+                CHF.tools.erase_hparams(self.name)
+            else: #create a new hparam json and model.h5
+                #set a new name
+                self.name = self.now
+                #new now to show the fact model already saved
+                self.now = datetime.now().strftime("%Y%m%d-%H%M%S")
+                self.hparams['name'] = self.name
+        else:
+            #to mark the fact that we will have a model already saved
+            
+            self.now = datetime.now().strftime("%Y%m%d-%H%M%S")
+        path = "./saved_models/" + self.name + ".h5"
+        #save model.h5
+        self.model.save_model(path)
+        #save hparams in json
+        CHF.tools.save_hparams(self.name, self.hparams)
+        return None
+    
     def _load_data(self, seed: int) -> None:
         """check if corresponding valid/train set already computed,
         if no compute and add the new data in DATA"""
@@ -105,130 +230,23 @@ class My_model:
         self.y_train = My_model.DATA[seed]['train_targets']
         self.normalization_mean = My_model.DATA[seed]['mean']
         self.normalization_std = My_model.DATA[seed]['std']
-
         return None
+    # lr loose (1-expdecay)*100 % evry rythm epoch
+    def lr_scheduler(self, epoch: int, lr: float) ->float: 
 
-    def slr_scheduler(self, epoch: int, lr: float) ->float:   
         if epoch % self.hparams['rythm'] == 0 and epoch > 0:
             return lr * self.hparams['learning_rate_decay']
         return lr
-
-    def init_callbacks(self) -> list:
-        
-        early_stop = EarlyStopping(
-            monitor='loss',          #mieux de monitorer la loss car moins variable 
-            min_delta=self.hparams['loss_delta_stop'],   #if nothing precise, it will check loss and stop after no change of >=0.1
-            patience=self.hparams['patience'], 
-            verbose=1,
-            restore_best_weights=True
-        ) 
-
-        # lr loose (1-expdecay)*100 % evry rythm epoch
-        #schedule == function to give to LearningScheduler, with epoch and lr as param
-        learningRateScheduler = LearningRateScheduler(self.lr_scheduler)
-        logdir = "./logs/hparam_tuning/" + self.name
-        tb_metrics = TensorBoard(logdir)
-        return [early_stop, learningRateScheduler, tb_metrics]
-
-    def create(self)->None:
-
-        self.callbacks = self.init_callbacks()
-
-        #que pr les nouvaux modeles
-        if self.name == self.now:
-            architecture = self.hparams['architecture']
-            for layer, neurons in enumerate(architecture[1:]):
-        
-                if layer == 0:
-                    self.model.add(Dense(
-                        neurons, 
-                        input_shape=(architecture[0],)
-                    ))
-                else:
-                    self.model.add(Dense(neurons))
-
-                self.model.add(Activation(LeakyReLU))
-
-                if  layer != len(architecture[1:]) - 1 :
-                    if self.hparams['batch_normalisation'] == True:
-                        self.model.add(BatchNormalization())
-                    
-                if layer == 0 or layer == 1:
-                    self.model.add(Dropout(self.hparams['dropout_rate']))
-            
-            opti = self.hparams['optimizer']
-            if opti == 'adam':
-                opti = Adam
-            else: opti = SGD
-
-            self.model.compile(
-                optimizer=opti(learning_rate=self.hparams['learning_rate']), 
-                loss=self.loss_function, 
-                metrics=self.other_metrics
-            )
-            
-        return None
-
-    def train(self) -> dict:
-        history = self.model.fit(
-            self.X_train, 
-            self.y_train,
-            shuffle=False,      #because already shuffled
-            validation_data=(self.X_val,self.y_val),
-            batch_size=self.hparams['batch_size'], 
-            epochs=self.hparams['max_epochs'],
-            callbacks=self.callbacks, #mpa_callback
-            verbose=1
-        ) 
-        return history 
-
-    def save(self, overwrite=False)->None:
-        """save model in .h5 format, to make predictions
-        save also the hparams of the model"""
-
-        if self.name != self.now: #already existing model
-            if overwrite:
-                #no need to change name, save the model on top on the old
-                #version but change hparams because performance might be diff
-                
-                CHF.tools.erase_hparams(self.name)
-            else: #create a new hparam json and model.h5
-                self.name = self.now
-                self.hparams['name'] = self.name
-        
-        path = "./saved_models/" + self.name + ".h5"
-        self.model.save_model(path)
-        CHF.tools.save_hparams(self.name, self.hparams)
-        return None
     
-    def load_my_model(self, model_name: str)->None:
-        """Create a model based on hparams or
-        use one based on path of previous model"""
-        #model already existing
-        if model_name != None:
-            path = "./models/" + model_name + ".h5"
+    def batch_nrmse(self, y_true, y_pred):
+        return K.sqrt(K.mean(K.square(y_pred - y_true)))/K.mean(y_true)
 
-            self.model = load_model(path)
-            self.hparams = CHF.tools.get_hparams_saved_model(model_name)
-            self.name = self.hparams['name']
-            
-            
-        #new
-        else:
-            self.name = self.now
-            self.model = Sequential()
-            
-        self.create()
-        #check if the corresponding vali/train set already computed
-        self._load_data(self.hparams['data_seed'])
-        return None
-    
-    
     def vizualize(self) -> None:
         """
         save a png file containing vizual rpz of the network
         """
         dimension = (100,100)
+        print("Model visualisation")
         CHF.tools.visualize_nn(
             self.model, 
             description=True, 
--- final/main.py
+++ final/main.py
@@ -2,16 +2,30 @@ import CHF_model_api as CHF
 
 if __name__ == '__main__':
 
+
     hparams = {
-        'data_seed': 1,
-        'name': None,
-        'weight_seed': 1,
-    }
+    'name': None,
+    'architecture':[5, 39, 40, 31, 42, 1],
+    'learning_rate': 0.001,
+    'dropout_rate' : 0.2,
+    'learning_rate_decay' : 0.96,
+    'rythm' : 15,
+    'alpha_acti' : 0,
+    'batch_size' : 32,
+    'optimizer' : 'adam',
+    'max_epochs' : 2,
+    'loss_function' : 'msle',
+    'loss_delta_stop' : 0.01,
+    'batch_normalisation' : True,
+    'patience': 15,
+    'data_seed' : 1,
+    'seed_tf': 1
+}
 
-    test = CHF.My_model(hparams)
-    test.create()
+    my_model = CHF.My_model(hparams)
+    my_model.vizualize()
 
-    print(CHF.My_model.DATA)
+    
 
     
 
